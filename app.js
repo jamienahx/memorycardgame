442acd39f96eaebb574ad66d89cefc54357cdb18
jamienahx <jamie.nahx@gmail.com>
2026-01-15 18:32:23 +0800

    Revised card flip animation logic explanation, defined first and second cards

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 //cached elements
 let cards  = document.querySelectorAll('.memory-card');
+let firstCard, secondCard;
+let hasFlippedCard= false;
 
 //functions 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    //toggle just adds if it is absent and removes if it is present: click --> add, click again --> remove
+    //add means add the class and it stays added until it is removed: click --> add, click again --> nothing happens
+
+    if (!hasFlippedCard) {
+        //!hasflipped card means it is no longer false -> the state of the card having been clicked
+        hasFlippedCard = true; 
+        firstCard = this;
+    } else {
+        //set it back to false, ready for the next "first click"
+        hasFlippedCard = false;
+        secondCard = this;
+    }
+
+
+    
 
 }
 
