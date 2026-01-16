b00ec68cf176cfb1c68d88b71f6c4b8174acb106
jamienahx <jamie.nahx@gmail.com>
2026-01-16 19:10:20 +0800

    some refactoring and edge cases

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let cards  = document.querySelectorAll('.memory-card');
 let firstCard, secondCard;
 let hasFlippedCard= false;
+let lockBoard =false;
 
 //functions 
 function flipCard() {
@@ -11,31 +12,53 @@ function flipCard() {
     //add means add the class and it stays added until it is removed: click --> add, click again --> nothing happens
 
     if (!hasFlippedCard) {
+        if(lockBoard) return ;
+        if(this===firstCard) return; //if the user clicks the first card again, return. note that on the true first iteration, this is undefined at this stage as firstCard= this comes later. 
         //!hasflipped card means it is no longer false -> the state of the card having been clicked
         hasFlippedCard = true; 
         firstCard = this;
-    } else {
+        return;
+    }
         //set it back to false, ready for the next "first click"
         hasFlippedCard = false;
         secondCard = this;
         checkForMatch()
-    }
 }
 
 function checkForMatch(){
     if(firstCard.dataset.framework===secondCard.dataset.framework) {
-        //disable click
+        disableCards();
+    } else {
+       unFlipCards();
+
+    }
+}
+
+function unFlipCards(){
+    lockBoard=true;
+     setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard=false;
+        resetBoard();
+    },1500);
+    
+}
+
+function disableCards(){
+            //disable click
         //remove event listener
         firstCard.removeEventListener('click',flipCard)
         secondCard.removeEventListener('click',flipCard)
-    } else {
-        //unflip the card
-        setTimeout(()=>{
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-    },1500);
+
+        resetBoard();
     
-    }
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+
 }
 
 
